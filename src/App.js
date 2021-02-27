dd5ef9bf58e7ab15b7fdc9eedec0f321186b9c79
Nicolas Gonzalez <nicolas.ignacio.gonzam@gmail.com>
2021-02-27 00:44:16 -0300

    Formularios

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Header from './components/Header'
 import Tasks from './components/Tasks'
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
+import AddTask from './components/AddTask'
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false)
   const [tasks, setTasks] = useState([
     {
         id: 1,
@@ -19,6 +21,12 @@ function App() {
 
 ])
 
+const addTask = (task) => {
+  const id = Math.floor(Math.random() * 10000)
+  const newTask = {id, ...task}
+  setTasks([...tasks, newTask])
+}
+
 const deleteTask = (id) => {
   setTasks(tasks.filter((task) => {
     return task.id !== id
@@ -29,8 +37,10 @@ const toggleReminder = (id) => {
 }
   return (
     <div className="container">
-     <Header />
-     {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} toggleReminder={toggleReminder}/>) : ('Nothing')}
+     <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask}/>
+     {showAddTask && <AddTask onAdd={addTask}/> }
+
+     {tasks.length > 0 ? (<Tasks tasks={tasks}  onDelete={deleteTask} toggleReminder={toggleReminder}/>) : ('Nothing')}
     </div>
   );
 }
