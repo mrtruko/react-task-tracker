bd155e65c8287dc36e005f5dc623980b5a47d4d5
Nicolas Gonzalez <nicolas.ignacio.gonzam@gmail.com>
2021-02-27 00:20:07 -0300

    Function ToggleReminder

--- src/App.js
+++ src/App.js
@@ -24,10 +24,13 @@ const deleteTask = (id) => {
     return task.id !== id
   }))
 }
+const toggleReminder = (id) => {
+  setTasks(tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task))
+}
   return (
     <div className="container">
      <Header />
-     {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask}/>) : ('Nothing')}
+     {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} toggleReminder={toggleReminder}/>) : ('Nothing')}
     </div>
   );
 }
